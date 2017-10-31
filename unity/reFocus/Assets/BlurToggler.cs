646a069ec16409ee8fe5a3b0062d1ccc247f49db
David Su <david.d.su@gmail.com>
2017-10-31 19:01:09 -0400

    create blur toggler that listens to server, as well as dummy server page

---
+++ unity/reFocus/Assets/BlurToggler.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityStandardAssets.ImageEffects;
+
+public class BlurToggler : MonoBehaviour {
+
+	public Blur blur;
+	public string serverURL;
+	public float checkServerInterval; // in seconds
+	public WWW serverResponse;
+
+	// Use this for initialization
+	void Start () {
+		// Disable blur
+		blur.enabled = false;
+
+		// Start checking server periodically
+		StartCoroutine ("CheckServer");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Activate/deactivate blur
+		if (serverResponse != null && serverResponse.isDone) {
+			// Check for errors
+			if (!string.IsNullOrEmpty(serverResponse.error)) {
+				Debug.Log (serverResponse.error);
+			}
+
+			// Check text and change blur accordingly
+			Debug.Log (serverResponse.text);
+			if (serverResponse.text == "true") {
+				blur.enabled = true;
+			}
+			else {
+				blur.enabled = false;
+			}
+		}
+	}
+
+	// Check the server every x seconds
+	IEnumerator CheckServer() {
+		while (true) {
+			yield return new WaitForSeconds (checkServerInterval);
+
+			// Do the actual checking
+			Debug.Log("Checking");
+			if (serverResponse == null || serverResponse.isDone) {
+				serverResponse = new WWW (serverURL);
+			}
+
+		}
+	}
+}
---
+++ unity/reFocus/dummy_blur_page.php
@@ -0,0 +1,3 @@
+<?php
+    echo time() % 2 == 0 ? "true" : "false";
+?>
